5bd1a9dfecc13a94deaa2eb77ce1f36f350fea95
Sloan Ahrens <sloan.ahrens@gmail.com>
2021-03-02 18:50:01 -0600

    new circleci image

--- bash-scripts/images/build-docker-images.sh
+++ bash-scripts/images/build-docker-images.sh
@@ -3,15 +3,14 @@
 set -e
 set -x
 
+# build vue.js images:
+docker build -t ui -f docker/ui/Dockerfile .
+docker build -t ui:dev -f docker/ui/Dockerfile.dev .
 
 # build Python base-image:
 docker build -t baseimage -f docker/base-python/Dockerfile .
 # build django image:
 docker build -t django -f docker/django/Dockerfile .
-
-# build vue.js images:
-docker build -t ui -f docker/ui/Dockerfile .
-docker build -t ui:dev -f docker/ui/Dockerfile.dev .
 
 # api-gateway microservice image
 docker build -t api-gateway -f docker/api-gateway/Dockerfile docker/api-gateway
