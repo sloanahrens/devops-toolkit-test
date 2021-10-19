ccacdc7fc526b1bdf2d9db683f7900603f5426ab
Sloan Ahrens <sloan.ahrens@gmail.com>
2021-10-19 08:07:34 -0500

    deleted cluster

--- kubernetes/us-east-2/dev/cluster/remote_state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt = true
-    bucket = "stellarbot-terraform-state-us-east-2-dev"
-    dynamodb_table = "stellarbot-terraform-state-us-east-2-dev"
-    region = "us-east-2"
-    key = "terraform.tfstate"
-  }
-}
